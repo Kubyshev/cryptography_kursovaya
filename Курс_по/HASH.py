c53b16af607e9bc7ed2991e0eeb9e6eb6bf78440
Kubyshev <117076814+Kubyshev@users.noreply.github.com>
2023-11-24 20:34:15 +0300

    Update HASH.py

--- Курс_по/HASH.py
+++ Курс_по/HASH.py
@@ -1,23 +1,63 @@
-n=1189
-m1=12
-m2=21
-m3=2
-m4=29
-m5=26
-m6=6
-m7=3
-h0=9
-h1=((h0+m1)**2)%1189
-print(h1)
-h2=((h1+m2)**2)%1189
-print(h2)
-h3=((h2+m3)**2)%1189
-print(h3)
-h4=((h3+m4)**2)%1189
-print(h4)
-h5=((h4+m5)**2)%1189
-print(h5)
-h6=((h5+m6)**2)%1189
-print(h6)
-h7=((h6+m7)**2)%1189
-print(h7)
+def formula(M: list[int], h0: int, n: int) -> list[int]:
+    h = [h0]
+    for mi in M:
+        h.append(((h[-1] + mi) ** 2) % n)
+    return h
+
+
+def make_table(alphabet: str) -> dict:
+    table: dict = {}
+    for i, char in enumerate(alphabet):
+        table[char] = i + 1
+    return table
+
+
+def get_M(word: str) -> list[int]:
+    M: list[int] = list()
+    for char in word:
+        M.append(table[char])
+    return M
+
+
+
+
+# p и q выбрать по варианту для Хэширования
+p: int = 41
+q: int = 29
+
+N: int = p * q
+
+h0: int = 9
+
+
+alphabet: str = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
+
+table: dict = make_table(alphabet)
+
+
+surname: str = "фамилия имя отчество"
+
+sur: str = surname.split(" ")[0]
+
+print("Фамилия:", surname)
+print(f"Введённое слово: {sur}")
+print(f"Длина слова: {len(sur)}")
+
+print("\n", "===" * 3, " Задание№3 Хэширование ", "===" * 3, "\n")
+
+
+M: list[int] = get_M(sur)
+
+h: int = formula(M, h0, N)
+
+H: int = h[-1]
+
+print("P:", p)
+print("Q:", q)
+print("n =", N)
+print("H[0] =", h0)
+print("M:", M)
+print("H от нулевого до последнего:", h)
+print("H",[len(sur)],"=",H)
+
+
